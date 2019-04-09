cafef94d63952d7cd7b8a050f9b25c61fda3a6fc
Andy Lindsay <andydlindsay@gmail.com>
2019-04-08 23:18:02 -0700

    add exercise three

--- exercise-three/app.js
+++ exercise-three/app.js
@@ -1,6 +1,20 @@
 new Vue({
-        el: '#exercise',
-        data: {
-            value: 0
+    el: '#exercise',
+    data: {
+        value: 0,
+        text: ''
+    },
+    computed: {
+        result: function() {
+            return this.value === 37 ? 'done' : 'not there yet';
         }
-    });
+    },
+    watch: {
+        value: function() {
+            const vm = this;
+            setTimeout(() => {
+                vm.value = 0;
+            }, 5000);
+        }
+    }
+});
