5582c418015ed02fb2f3c241461b55d7aeda1f6d
Andy Lindsay <andydlindsay@gmail.com>
2019-04-15 16:18:37 -0700

    add communication module

---
+++ communication/src/components/User.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeMyName">Change My Name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :name="myName" :age="age" :resetName="resetName"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit :age="age" @ageWasEdited="age = $event"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function() {
+            return {
+                myName: 'Maximillian',
+                age: 27
+            };
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        },
+        methods: {
+            changeMyName: function() {
+                this.myName = 'Terrence';
+            },
+            resetName: function() {
+                this.myName = 'Maximillian';
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ communication/src/components/UserDetail.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>My name is {{ name }}</p>
+        <p>My age is {{ age }}</p>
+        <p>Many Details</p>
+        <button @click="resetName">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            'name': {
+                type: String,
+                required: true
+            },
+            'resetName': Function,
+            'age': Number
+        }
+    };
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
