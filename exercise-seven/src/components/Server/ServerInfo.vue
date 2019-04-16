ae8face33cbcc6700b02697b34cdb32c39a2288d
Andy Lindsay <andydlindsay@gmail.com>
2019-04-16 10:19:56 -0700

    add exercise seven

---
+++ exercise-seven/src/components/Server/ServerInfo.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>
+        Server #{{ server.number }} is {{ server.status }}
+    </p>
+</template>
+
+<script>
+    export default {
+        props: ['server']
+    }
+</script>
+
+<style>
+
+</style>
---
+++ exercise-seven/src/components/Server/Servers.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <li
+                class="list-group-item"
+                v-for="server in servers"
+                :key="server.number"
+            >
+                <app-server-info :server="server"></app-server-info>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import ServerInfo from './ServerInfo.vue';
+    export default {
+        data: function() {
+            return {
+                servers: [
+                    { number: 1, status: 'Normal' },
+                    { number: 2, status: 'Critical' },
+                    { number: 3, status: 'Unknown' },
+                    { number: 4, status: 'Normal' }
+                ]
+            };
+        },
+        components: {
+            'app-server-info': ServerInfo
+        }
+    }
+</script>
+
+<style>
+
+</style>
